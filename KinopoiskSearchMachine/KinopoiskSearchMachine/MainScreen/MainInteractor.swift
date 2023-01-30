820b0626245d36cf3e07f3b2138ba30c79590791
Emil Shpeklord <emil42661@gmail.com>
2023-01-30 20:33:49 +0300

    network manager is here

--- KinopoiskSearchMachine/KinopoiskSearchMachine/MainScreen/MainInteractor.swift
+++ KinopoiskSearchMachine/KinopoiskSearchMachine/MainScreen/MainInteractor.swift
@@ -11,23 +11,16 @@ protocol MainBusinessLogic {
 
 final class MainInteractor: MainBusinessLogic, MainDataStore {
     private let presenter: MainPresentationLogic
-    let provider = MoyaProvider<MovieType>()
 
     init(presenter: MainPresentationLogic) {
         self.presenter = presenter
     }
 
     func requestInitForm(_ request: Main.InitForm.Request) {
-        provider.request(.bestMovies(page: 1)) { result in
+        NetworkManager.getTopSeries(page: 1) { result in
             switch result {
-            case .success(let response):
-                print(response.response)
-                do {
-                    let json = try JSONDecoder().decode(Movies.self, from: response.data)
-                    print(json)
-                } catch {
-                    print(error.localizedDescription)
-                }
+            case .success(let movies):
+                print(movies)
             case .failure(let error):
                 print(error.localizedDescription)
             }
--- KinopoiskSearchMachine/KinopoiskSearchMachine/Network/NetworkManager.swift
+++ KinopoiskSearchMachine/KinopoiskSearchMachine/Network/NetworkManager.swift
@@ -9,5 +9,21 @@ import Foundation
 import Moya
 
 struct NetworkManager {
-    fileprivate let provider = MoyaProvider<MovieType>()
+    static let provider = MoyaProvider<MovieType>()
+
+    static func getTopSeries(page: Int, completion: @escaping (Result<Movies, Error>) -> Void) {
+        provider.request(.bestMovies(page: page)) { result in
+            switch result {
+            case .success(let response):
+                do {
+                    let movies = try JSONDecoder().decode(Movies.self, from: response.data)
+                    completion(.success(movies))
+                } catch {
+                    completion(.failure(error))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
 }
